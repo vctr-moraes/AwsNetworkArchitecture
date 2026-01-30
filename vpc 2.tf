b71c23d12ab4b8a83d79addc7879ea8e2436043c
Victor Moraes <victor.moraes@outlook.com.br>
2026-01-29 22:37:10 -0300

    Adds internet gateway and subnet to VPC B
    Introduces an internet gateway, public route table, subnet, and their associations for VPC B to enable external connectivity and proper routing. Improves network architecture by supporting public resources and inter-VPC communication.

--- vpc 2.tf
+++ vpc 2.tf
@@ -8,10 +8,25 @@ resource "aws_vpc" "main_b" {
   }
 }
 
+# Internet Gateway
+
+resource "aws_internet_gateway" "internet_gateway_main_b" {
+  vpc_id = aws_vpc.main_b.id
+
+  tags = {
+    Name = "IGW-AnyCompany-2"
+  }
+}
+
 # Route Tables
 
 resource "aws_route_table" "route_table_public_anycompany_2" {
   vpc_id = aws_vpc.main_b.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway_main_b.id
+  }
 
   route {
     cidr_block                = "172.41.0.0/16"
@@ -30,4 +45,22 @@ resource "aws_main_route_table_association" "route_table_public_anycompany_2" {
   route_table_id = aws_route_table.route_table_public_anycompany_2.id
 }
 
-// criar uma subrede para a VPC B
+# Subnets
+
+resource "aws_subnet" "subnet_anycompany_2_a" {
+  vpc_id               = aws_vpc.main_b.id
+  cidr_block           = "172.51.0.0/20"
+  availability_zone_id = "usw1-az3"
+
+  tags = {
+    Name = "Subnet-A"
+    VPC  = "VPC-AnyCompany-2"
+  }
+}
+
+# Associations Subnet x Route Table
+
+resource "aws_route_table_association" "route_table_association_anycompany_2" {
+  subnet_id      = aws_subnet.subnet_anycompany_2_a.id
+  route_table_id = aws_route_table.route_table_public_anycompany_2.id
+}
